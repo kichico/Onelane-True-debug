13b55db9319eb55f1d46d0f3bebd22127db90fb1
kichico <suekiti3297@gmail.com>
2020-11-30 17:33:22 +0900

    add the result of multilane

---
+++ スパコンデバッグ用/Result/columnchange.py
@@ -0,0 +1,6 @@
+﻿import pandas as pd
+
+file='test_anowar.csv'
+df=pd.read_csv(file)
+for i in range(200)
+    if i%20==0
---
+++ スパコンデバッグ用/Result/drawSpacionaldiagram.py
@@ -0,0 +1,16 @@
+﻿import pandas as pd
+import matplotlib.pyplot as plt
+
+File_dist = 'Global1_distributed.dat'
+File_Normal='Global1.csv'
+df_distributed = pd.read_csv(File_dist)
+df_fixed=pd.read_csv(File_Normal)
+df_pivot_distributed = df_distributed.pivot_table(values='density', index='flux')
+df_pivot_fixed=df_fixed.pivot_table(values='density', index='flux')
+ax=df_distributed.plot.scatter(x='density',y='flux',s=5,color='#ff450099',label='distributed(4~6)')
+df_fixed.plot.scatter(x='density',y='flux',s=5,color='#4169e199',label='fixed(5)',ax=ax)
+plt.title('fundamentaldaigram,compare,idletime = 1800ts')
+plt.legend(bbox_to_anchor=(1,1),loc='upper right',borderaxespad=0,fontsize=15)
+plt.savefig('fundamentaldaigram_both_Vmax_1800ts.png',dpi=300)
+plt.show()
+
